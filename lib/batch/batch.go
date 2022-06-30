ed4cb4f9dee97aac29022b3901b9adf9cb74d542
Pavel Zhuravlev <zhuravlev.pe@gmail.com>
2022-06-30 23:56:44 +0700

    solution

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
@@ -13,6 +14,51 @@ func getOne(id int64) user {
 	return user{ID: id}
 }
 
+func getUsers(s, n int64, ch chan<- user, wg *sync.WaitGroup) {
+	defer wg.Done()
+	last := s + n
+	for ; s < last; s++ {
+		ch <- getOne(s)
+	}
+}
+
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	if pool < 1 {
+		pool = 1
+	}
+	if pool > n {
+		pool = n
+	}
+	ch := make(chan user)
+	wg := sync.WaitGroup{}
+	perPool := n / pool
+	var startId int64
+	for i := 0; i < int(pool); i++ {
+		batchCount := n - startId
+		batchCount = min(perPool, batchCount)
+		s := startId
+		wg.Add(1)
+		go getUsers(s, batchCount, ch, &wg)
+
+		startId += perPool
+	}
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	result := make([]user, 0, n)
+	for u := range ch {
+		result = append(result, u)
+	}
+
+	return result
+}
+
+func min(a, b int64) int64 {
+	if a < b {
+		return a
+	}
+	return b
 }
